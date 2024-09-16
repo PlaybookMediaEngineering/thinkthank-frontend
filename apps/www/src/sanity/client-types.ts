4084e916a11e3376c931040e91ffc41538541276
Lens <45818877+yoanyombapro1234@users.noreply.github.com>
2024-09-16 02:44:36 -0400

    feat: add privacy and terms
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>

--- apps/www/src/sanity/client-types.ts
+++
@@ -1,8 +0,0 @@
-import '@sanity/client'
-
-declare module '@sanity/client' {
-  interface SanityQueries {
-    'count(*[\n  _type == "post"\n  && defined(slug.current)\n  && (isFeatured != true || defined($category))\n  && select(defined($category) => $category in categories[]->slug.current, true)\n])': TOTAL_POSTS_QUERYResult
-    // ... other query types ...
-  }
-}
